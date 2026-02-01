c5b56731a6964ed79309a4ba142e11d1fbdbd48b
Jahidul Islam <ionzahid0987@gmail.com>
2026-02-01 22:21:38 +0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}", // <--- IMPORTANT: Ensure this path matches your files
   ],
   theme: {
     extend: {},
